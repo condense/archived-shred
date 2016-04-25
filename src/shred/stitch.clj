e7ec1eb80551f5fde89463198fb2dd126fec3c17
Oliver George <oliver@condense.com.au>
2016-04-25 19:19:26 +1000

    Naive stitch

--- src/shred/stitch.clj
+++ src/shred/stitch.clj
@@ -1,1 +1,11 @@
 (ns shred.stitch)
+
+
+(defn stitch [ms ns path km many?]
+  (let [get-ks (fn [m] (map #(get-in m %) (keys km)))
+        get-vs (fn [m] (map #(get    m %) (vals km)))
+        lookup (group-by get-vs ns)]
+    (for [m ms]
+      (let [values (get lookup (get-ks m))
+            values (if-not many? (first values) values)]
+        (assoc-in m path values)))))
